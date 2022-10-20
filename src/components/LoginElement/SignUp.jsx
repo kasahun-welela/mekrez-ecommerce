a1382c5ac27c8eb3d114a0a70a4db69268c8f064
kasahun-welela <kasahunwelela1@gmail.com>
2022-10-20 09:05:25 +0100

    state added

--- src/components/LoginElement/SignUp.jsx
+++ src/components/LoginElement/SignUp.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SignUp() {
+  const [signUpData, setSignUpData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+    cPassword: "",
+  });
+
+  function handleSignUp(event) {
+    const { name, value } = event.target;
+    // const name = event.target.name;
+    // const value = event.target.value;
+    setSignUpData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+    console.log(signUpData);
+  }
+
+  function submitData(event) {
+    let formData = {};
+    formData = signUpData;
+    console.log(formData);
+  }
+
   return (
     <div className=" mt-4">
       {/* // <div className="bg-grey-900 min-h-screen mt-0 flex flex-col md:-mt-20"> */}
@@ -9,33 +33,39 @@ function SignUp() {
           <h1 className="mb-8 text-3xl text-center">Sign up</h1>
           <input
             type="text"
-            className="block border border-grey-light w-full p-3 rounded mb-4"
+            className="block border border-grey-light w-full  p-3 rounded mb-4"
             name="fullName"
             placeholder="Full Name"
+            onChange={handleSignUp}
           />
-
+          <p className="mb-1 text-center ">{signUpData.fullName}</p>
           <input
             type="text"
             className="block border border-grey-light w-full p-3 rounded mb-4"
             name="email"
             placeholder="Email"
+            onChange={handleSignUp}
           />
-
+          <p className="mb-1 text-center ">{signUpData.email}</p>
           <input
             type="password"
             className="block border border-grey-light w-full p-3 rounded mb-4"
             name="password"
             placeholder="Password"
+            onChange={handleSignUp}
           />
+          <p className="mb-1 text-center ">{signUpData.password}</p>
           <input
             type="password"
             className="block border border-grey-light w-full p-3 rounded mb-4"
-            name="confirm_password"
+            name="cPassword"
             placeholder="Confirm Password"
+            onChange={handleSignUp}
           />
-
+          <p className="mb-1 text-center ">{signUpData.cPassword}</p>
           <button
             type="submit"
+            onClick={submitData}
             className="w-full bg-blue-700 text-center py-3 rounded bg-green text-white hover:bg-blue-500 focus:outline-none my-1"
           >
             Create Account
